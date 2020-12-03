dbaacc1aa10b41fc83cf3680a3427bce97199ab7
Albert Vernon <avernon@singlestore.com>
2020-12-03 12:01:18 -0500

    Insert Git version and date when building

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sed -i "s/%VERSION%/$(git describe --abbrev --always --dirty)/" book.md
+sed -i.bak "s/%VERSION%/Version $(git describe --abbrev --always --dirty) -- $(date '+%B %d, %Y')/" book.md
 pandoc --to=epub3 --toc --default-image-extension=png -o book.epub book.md
 pandoc            --toc --default-image-extension=png -o book.pdf book.md
+mv book.md.bak book.md
